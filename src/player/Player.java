1ad0b4663d2612d0ff5848ebf99e62b14ae24c6e
Reydarver <reydarver@gmail.com>
2018-01-20 17:06:36 +0100

    Execución permanente y comparación de Timestamp

--- src/player/Player.java
+++ src/player/Player.java
@@ -10,7 +10,11 @@ import org.json.*;
 
 import classes.Kline;
 import classes.Period;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
@@ -20,83 +24,119 @@ public class Player {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws FileNotFoundException, IOException 
     {     
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Period period_9 = new Period(9);
         Period period_21 = new Period(21);
         Period period_40 = new Period(40);
         
-        try {
-            URL oracle = new URL("https://www.binance.com/api/v1/klines?symbol=QTUMETH&interval=15m"); // URL to Parse
-            URLConnection yc = oracle.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        String jsonTimestamp = "lastTimestamp.json";
+        
+        while(true)
+        {
+            long currentTimestamp = System.currentTimeMillis();
+            long lastKlineEndTimestamp = getJsonLastTimestamp(jsonTimestamp);
             
-            // Read all the input lines from the URL (in this case just 1)
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                
-                // Parse the full JSON
-                JSONArray j_rows = new JSONArray(inputLine);
-                for(int i = 0; i < j_rows.length(); i++) 
-                {
-                    JSONArray j_rowData = new JSONArray(j_rows.get(i).toString());
-                    
-                    Kline kline = new Kline(j_rowData.getLong(0), j_rowData.getString(1), j_rowData.getString(2), j_rowData.getString(3), j_rowData.getString(4), 
-                            j_rowData.getString(5), j_rowData.getLong(6), j_rowData.getString(7), j_rowData.getInt(8), j_rowData.getString(9), j_rowData.getString(10));
-                    
-                    System.out.println(dateFormat.format(kline.getStartTime()));
-                    
-                    period_9.addKline(kline);
-                    period_21.addKline(kline);
-                    period_40.addKline(kline);
-                    
-                    // Where we have enough data...
-                    if(period_9.periodIsReady() && period_21.periodIsReady() && period_40.periodIsReady()) 
+            System.out.println(lastKlineEndTimestamp);
+            
+            try {
+                System.out.println("URLConnection");
+                URL oracle = new URL("https://www.binance.com/api/v1/klines?symbol=QTUMETH&interval=15m"); // URL to Parse
+                URLConnection yc = oracle.openConnection();
+                BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+                // Read all the input lines from the URL (in this case just 1)
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+
+                    // Parse the full JSON
+                    JSONArray j_rows = new JSONArray(inputLine);
+                    for(int i = 0; i < j_rows.length(); i++) 
                     {
-//                        System.out.println(period_9.getActualMean());
-//                        System.out.println(period_21.getActualMean());
-//                        System.out.println(period_40.getActualMean());
-                        
-                        // Check period_40 is the lowest
-                        boolean period_40oldMeansmallest = (period_40.getOldMean() < period_21.getOldMean()) &&(period_40.getOldMean() < period_9.getOldMean());
-                        boolean period_40actualMeansmallest = (period_40.getActualMean()< period_21.getActualMean()) &&(period_40.getActualMean() < period_9.getActualMean());
-                        if(period_40oldMeansmallest && period_40actualMeansmallest)
+                        JSONArray j_rowData = new JSONArray(j_rows.get(i).toString());
+
+                        Kline kline = new Kline(j_rowData.getLong(0), j_rowData.getString(1), j_rowData.getString(2), j_rowData.getString(3), j_rowData.getString(4), 
+                                j_rowData.getString(5), j_rowData.getLong(6), j_rowData.getString(7), j_rowData.getInt(8), j_rowData.getString(9), j_rowData.getString(10));
+
+                        period_9.addKline(kline);
+                        period_21.addKline(kline);
+                        period_40.addKline(kline);
+
+                        // Where we have enough data...
+                        if(period_9.periodIsReady() && period_21.periodIsReady() && period_40.periodIsReady()) 
                         {
-                            // Eloi's case 1 - Sell
-                            boolean period_9old_over_period21_old = (period_9.getOldMean() > period_21.getOldMean());
-                            boolean period_21actual_over_period9_actual = (period_21.getActualMean() > period_9.getActualMean());
-                            if(period_9old_over_period21_old && period_21actual_over_period9_actual)
+    //                        System.out.println(period_9.getActualMean());
+    //                        System.out.println(period_21.getActualMean());
+    //                        System.out.println(period_40.getActualMean());
+
+                            if(currentTimestamp > kline.getStartTime() && currentTimestamp < kline.getEndTime())
                             {
-                                System.out.println("SELL");
-                            }
-                            
-                            // Eloi's case 5 - Sell
-                            boolean period_21old_over_period9_old = (period_21.getOldMean() > period_9.getOldMean());
-                            boolean period_9actual_over_period21_actual = (period_9.getActualMean() > period_21.getActualMean());
-                            if(period_21old_over_period9_old && period_9actual_over_period21_actual)
-                            {
-                                System.out.println("BUY");
-                            }
+                                System.out.println("in " + dateFormat.format(kline.getStartTime()));
+                                
+                                // Check period_40 is the lowest
+                                boolean period_40oldMeansmallest = (period_40.getOldMean() < period_21.getOldMean()) &&(period_40.getOldMean() < period_9.getOldMean());
+                                boolean period_40actualMeansmallest = (period_40.getActualMean()< period_21.getActualMean()) &&(period_40.getActualMean() < period_9.getActualMean());
+                                if(period_40oldMeansmallest && period_40actualMeansmallest)
+                                {
+                                    // Eloi's case 1 - Sell
+                                    boolean period_9old_over_period21_old = (period_9.getOldMean() > period_21.getOldMean());
+                                    boolean period_21actual_over_period9_actual = (period_21.getActualMean() > period_9.getActualMean());
+                                    if(period_9old_over_period21_old && period_21actual_over_period9_actual)
+                                    {
+                                        System.out.println("SELL");
+                                    }
+
+                                    // Eloi's case 5 - Sell
+                                    boolean period_21old_over_period9_old = (period_21.getOldMean() > period_9.getOldMean());
+                                    boolean period_9actual_over_period21_actual = (period_9.getActualMean() > period_21.getActualMean());
+                                    if(period_21old_over_period9_old && period_9actual_over_period21_actual)
+                                    {
+                                        System.out.println("BUY");
+                                    }
+                                } 
+
+                                // Set the last timestamp
+                                JSONObject obj = new JSONObject();
+                                obj.put("Timestamp", kline.getEndTime());
+                                try (FileWriter file = new FileWriter(jsonTimestamp)) {
+                                    file.write(obj.toString());
+                                }
+                            } //endIfTimestamp     
                         }
-                        
+
+
                     }
-                    
-                    // Set the last timestamp
-                    JSONObject obj = new JSONObject();
-                    obj.put("Timestamp", kline.getEndTime());
-                    try (FileWriter file = new FileWriter("test.json")) {
-                        file.write(obj.toString());
-                    }
-                    
-                    Thread.sleep(1 * 60 * 1000); // milliseconds to a second
                 }
+                in.close();
+
+                Thread.sleep(1 * 60 * 1000); // milliseconds to a second
+
+            } catch(Exception e) {
+                System.err.println(e);
             }
-            in.close();
             
-        } catch(Exception e) {
-            System.err.println(e);
+        } //endWhileTrue
+        
+    }
+
+    private static long getJsonLastTimestamp(String filename) throws FileNotFoundException, IOException 
+    {
+        File f = new File(filename);
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        String line;
+        if((line = br.readLine()) != null)
+        {
+            JSONObject json = new JSONObject(line);
+            System.out.println(json.toString());
+            return json.getLong("Timestamp");
+        }
+        else 
+        {
+            System.out.println("holaa");
+            return 0;
         }
     }
     
